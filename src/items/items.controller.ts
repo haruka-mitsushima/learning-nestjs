ef8674b2bf0bb6c9261c46e26fa18bac5b74e13c
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-12 09:01:38 +0900

    CRUDの実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 import { ItemsService } from './items.service';
 
 @Controller('items')
@@ -6,7 +16,39 @@ export class ItemsController {
   constructor(private readonly ItemsService: ItemsService) {}
 
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.ItemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.ItemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.ItemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.ItemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    return this.ItemsService.delete(id);
   }
 }
