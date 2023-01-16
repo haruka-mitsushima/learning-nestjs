163cb76f13f8d8c282d69ffa0578d872aba744f7
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-16 11:10:34 +0900

    ログイン機能の実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 import { User } from 'src/entities/user.entity';
 import { AuthService } from './auth.service';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,12 @@ export class AuthController {
   @Post('signup')
   async signup(@Body() createuserDto: CreateUserDto): Promise<User> {
     return await this.authService.signUp(createuserDto);
+  }
+
+  @Post('signin')
+  async signIn(
+    @Body() credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.signin(credentialsDto);
   }
 }
