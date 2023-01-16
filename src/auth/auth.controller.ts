2d427ed71ebd3250b2e9377cec8f98783b377ec0
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-16 10:11:01 +0900

    ユーザー作成機能の実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
+import { User } from 'src/entities/user.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createuserDto: CreateUserDto): Promise<User> {
+    return await this.authService.signUp(createuserDto);
+  }
+}
